32a7c01758b2ae7d2068781d3be07704f0ac7179
Grant Southey <grant.southey@consensys.net>
2022-10-13 11:00:08 +0100

    Add EIP1559 Transaction sample

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ var mnemonicVue = new Vue({
         privKey: "",
         pubKey: "",
         ETHaddress: "",
-        sampleTransaction: {
+        sampleLegacyTransaction: {
             nonce: '0x00',
             gasPrice: '0x09184e72a000', 
             gasLimit: '0x2710',
@@ -28,16 +28,33 @@ var mnemonicVue = new Vue({
             data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057',
             chainId: 3
         },
+        sampleEIP1559Transaction: {
+            nonce: '0x00',
+            type: 2,
+            maxPriorityFeePerGas: '0x09184e72a000',
+            maxFeePerGas: '0x09184e72a000',
+            gasLimit: '0x2710',
+            to: '0x31c1c0fec59ceb9cbe6ec474c31c1dc5b66555b6', 
+            value: '0x10', 
+            data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057',
+            chainId: 3
+        },
         signedSample: {},
-        recoveredAddress: ""
+        signedEIP1559Sample: {},
+        recoveredAddress: "",
+        recoveredEIP1559Address: ""
     },
     methods:{
         generateNew: function(){
             this.mnemonic = generateMnemonic()
         },
         signSampleTx: function(){
-            this.signedSample = signTx(this.privKey, this.sampleTransaction)
-            console.log("signed Sample", this.signedSample)
+            this.signedSample = signTx(this.privKey, this.sampleLegacyTransaction)
+            console.log("signed Legacy Sample", this.signedSample)
+        },
+        signSampleEIP1559Tx: function(){
+            this.signedEIP1559Sample = signTx(this.privKey, this.sampleEIP1559Transaction)
+            console.log("signed 1559 Sample", this.sampleEIP1559Transaction)
         }
     },
     watch: {
